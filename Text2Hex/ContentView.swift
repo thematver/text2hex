2ebee68dd76c339c584190cb69f47057ec4059b2
Матвей Корепанов <anomatver@icloud.com>
2021-12-17 22:09:38 +0300

    Initial Commit

---
+++ Text2Hex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Text2Hex
+//
+//  Created by Менетиос on 17.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Text2Hex/Text2HexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text2HexApp.swift
+//  Text2Hex
+//
+//  Created by Менетиос on 17.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct Text2HexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
